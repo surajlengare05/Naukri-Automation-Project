0f062e3e7ab202ab322a66364246a3322841a45c
surajlengare05 <lengaresuraj@gmail.com>
2025-05-18 17:25:13 +0530

    chromeoptions with sleep 2 sec

--- src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
+++ src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
@@ -33,7 +33,7 @@ public class TC_ProfilePageTest
 	{
 		WebDriverManager.chromedriver().setup();
 		// Initialize ChromeOptions
-       /* ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless"); // Run in headless mode
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
@@ -41,7 +41,7 @@ public class TC_ProfilePageTest
         options.addArguments("--window-size=1920,1080");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
         options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false); */
+        options.setExperimentalOption("useAutomationExtension", false); 
         
 		driver = new ChromeDriver();
 		
@@ -50,7 +50,7 @@ public class TC_ProfilePageTest
 		
 		driver.get("https://www.naukri.com/");
 		System.out.println("Url opened");
-		Thread.sleep(8000);
+		Thread.sleep(2000);
 	}
 	
 	
