544c33c8393bbc55ad755c65a393c0cc9c7af1f5
surajlengare05 <lengaresuraj@gmail.com>
2025-05-18 14:15:31 +0530

    reverting back to jenkins

--- src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
+++ src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
@@ -33,7 +33,7 @@ public class TC_ProfilePageTest
 	{
 		WebDriverManager.chromedriver().setup();
 		// Initialize ChromeOptions
-        ChromeOptions options = new ChromeOptions();
+        /*ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless"); // Run in headless mode
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
@@ -41,18 +41,16 @@ public class TC_ProfilePageTest
         options.addArguments("--window-size=1920,1080");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
         options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("useAutomationExtension", false);*/
         
-		driver = new ChromeDriver(options);
+		driver = new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.naukri.com/");
 		System.out.println("Url opened");
-		Thread.sleep(5000);
-		System.out.println(driver.getPageSource());
-
+		Thread.sleep(2000);
 	}
 	
 	
@@ -63,7 +61,7 @@ public class TC_ProfilePageTest
 		LoginPage loginPg = new LoginPage(driver);
 		
 		loginPg.clickLoginLink();
-		System.out.println("clickd on login link");
+		System.out.println("clicked on login link");
 		loginPg.enterEmail("lengaresuraj@gmail.com");
 		loginPg.enterPassword("Suraj@123");
 		loginPg.clickLoginBtn();
