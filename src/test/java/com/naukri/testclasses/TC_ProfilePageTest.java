19be1ca5fdd25157483505677010be11ce8f05fe
surajlengare05 <lengaresuraj@gmail.com>
2025-05-18 18:54:39 +0530

    change in chromeoptions

--- src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
+++ src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
@@ -38,8 +38,8 @@ public class TC_ProfilePageTest
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
         options.addArguments("--disable-gpu"); // Applicable to Windows OS only
-        /*options.addArguments("--window-size=1920,1080");
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
+        options.addArguments("--window-size=1920,1080");
+        /*options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
         options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
         options.setExperimentalOption("useAutomationExtension", false); */
         
