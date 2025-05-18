4eb0327930b91a6cdddfbeec206cc2fc1d64e965
surajlengare05 <lengaresuraj@gmail.com>
2025-05-18 19:31:01 +0530

    eady to schedule

--- src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
+++ src/test/java/com/naukri/testclasses/TC_ProfilePageTest.java
@@ -40,9 +40,7 @@ public class TC_ProfilePageTest
         options.addArguments("--disable-gpu"); // Applicable to Windows OS only
         options.addArguments("--window-size=1920,1080");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
-        /*options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false); */
-        
+                
 		driver = new ChromeDriver(options);
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
